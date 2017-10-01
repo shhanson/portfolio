a0e2ae6a59d1a8338efbfb15ed3df08ceccdf928
shhanson <sarahh.hanson@gmail.com>
2017-10-01 12:41:44 -0500

    still debugging

--- script.js
+++ script.js
@@ -1,30 +1,19 @@
 $(document).ready(function(){
 
-  const touchEvent = 'ontouchstart' in window ? 'touchstart' : 'click';
 
   $('.materialboxed').materialbox();
 
   $('*').css('cursor', 'pointer');
 
-  // $(document).on(touchEvent, '#meetrunnerLink', () => {
-  //   $('.project').hide();
-  //   $('#meetrunnerProject').show();
-  // });
-  const meetrunner = document.getElementById('meetrunnerLink');
-  meetrunner.addEventListener('click', function(){
-    $('.project').hide();
-    $('#meetrunnerProject').show();
-  });
-
-  // $(document).on(touchEvent, '#meetrunnerLink', function(){
-  //   $('.project').hide();
-  //   $('#meetrunnerProject').show();
-  // });
 
   // $('#meetrunnerLink').click(() => {
   //   $('.project').hide();
   //   $('#meetrunnerProject').show();
   // });
+  $('#meetrunnerLink').on('click', function(){
+      $('.project').hide();
+      $('#meetrunnerProject').show();
+  });
 
 
   $('#budgieLink').click(() => {
