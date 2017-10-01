f3c1d2dd301d4bddd968d393e83337d0f429ef3a
shhanson <sarahh.hanson@gmail.com>
2017-10-01 11:54:30 -0500

    Attempting to fix iOS click event error

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ $(document).ready(function(){
 
   $('.materialboxed').materialbox();
 
+  $('.projectLink').css('cursor', 'pointer');
+
   $('#meetrunnerLink').click(() => {
     $('.project').hide();
     $('#meetrunnerProject').show();
   });
+
 
   $('#budgieLink').click(() => {
     $('.project').hide();
